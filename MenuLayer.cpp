729c548d1bc6610ed52ec436e18b2437e888784e
h1994st <h1994st@gmail.com>
2013-09-22 19:45:01 +0800

    Menulayer

--- MenuLayer.cpp
+++ MenuLayer.cpp
@@ -7,29 +7,8 @@
 
 #include "MenuLayer.h"
 
-<<<<<<< HEAD
 using namespace cocos2d;
 
-=======
- // MenuLayer::MenuLayer(){
-	// setTouchEnabled(true);
-	// setTouchPriority(kCCMenuHandlerPriority + 1);
-	// setTouchMode(kCCTouchesOneByOne);
-	// CCSprite* startSpriteNormal = CCSprite::create("play_button.png");
-	// CCMenuItemSprite* item1 = CCMenuItemSprite::create(startSpriteNormal, startSpriteNormal, startSpriteNormal,
-	// 		this, menu_selector(MenuLayer::startCallback) );
-	// CCSprite* quitSpriteNormal =  CCSprite::create("quit_button.png");
-	// CCMenuItemSprite* item2 = CCMenuItemSprite::create(quitSpriteNormal, quitSpriteNormal, quitSpriteNormal,
-	// 			this, menu_selector(MenuLayer::startCallback) );
-	// CCMenu* menu = CCMenu::create( item1, item2,NULL);
-	// menu->alignItemsVertically();
-
-	// addChild(menu);
-	// CCSize winSize = CCDirector::sharedDirector()->getWinSize();
-	// menu->setPosition(ccp(winSize.width/2, winSize.height/2));
-
-// }
->>>>>>> master
 void MenuLayer::initBackground()
 {
 	CCSize size = CCDirector::sharedDirector()->getWinSize();
@@ -40,7 +19,6 @@ void MenuLayer::initBackground()
 
 	// 设置图片精灵的位置
 	pSprite->setPosition(ccp(size.width/2, size.height/2));
-<<<<<<< HEAD
 
 	// 把图片精灵放置在图层中
 	this->addChild(pSprite, 0);
@@ -124,76 +102,7 @@ bool MenuLayer::init()
 	this->initMenu();
 
 	return true;
-=======
-
-	// 把图片精灵放置在图层中
-	this->addChild(pSprite, 0);
->>>>>>> master
 }
-
-
-void MenuLayer::initMenu()
-{
-	//Get windows size
-	CCSize size = CCDirector::sharedDirector()->getWinSize();
-
-	//Start
-	CCMenuItemImage *pStartItem = CCMenuItemImage::create(
-														  "Start.png",
-														  "Start_Pressed.png",
-														  this,
-														  menu_selector(MenuLayer::menuStart));
-	pStartItem -> setPosition( ccp(0, 0) );
-
-	CCMenu* pMenusStart = CCMenu::create(pStartItem, NULL);
-	pMenusStart -> setPosition( ccp(size.width / 2, size.height / 2 + 152) );
-	this -> addChild(pMenusStart, 1);
-
-	//Help
-	CCMenuItemImage *pHelpItem = CCMenuItemImage::create(
-														  "Help.png",
-														  "Help_Pressed.png",
-														  this,
-														  menu_selector(MenuLayer::menuStart));
-	pHelpItem -> setPosition( ccp(0, 0) );
-
-	CCMenu* pMenusHelp = CCMenu::create(pHelpItem, NULL);
-	pMenusHelp -> setPosition( ccp(size.width / 2, size.height / 2) );
-	this -> addChild(pMenusHelp, 1);
-
-	//Quit
-	CCMenuItemImage *pQuitItem = CCMenuItemImage::create(
-														  "Quit.png",
-														  "Quit_Pressed.png",
-														  this,
-														  menu_selector(MenuLayer::menuCloseCallback));
-	pQuitItem -> setPosition( ccp(0, 0) );
-
-	CCMenu* pMenusQuit = CCMenu::create(pQuitItem, NULL);
-	pMenusQuit -> setPosition( ccp(size.width / 2, size.height / 2 - 152) );
-	this -> addChild(pMenusQuit, 1);
-
-//	//Close
-//	CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-//											"CloseNormal.png",
-//											"CloseSelected.png",
-//											this,
-//											menu_selector(MainScene::menuCloseCallback));
-//	pCloseItem->setPosition( ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20) );
-//	CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-//	pMenu->setPosition( CCPointZero );
-//	this->addChild(pMenu, 1);
-}
-
-
-
-
-
-
-
-
-
-
 
 CCScene* MenuLayer::scene()
 {
@@ -203,7 +112,6 @@ CCScene* MenuLayer::scene()
 	return scene;
 }
 
-<<<<<<< HEAD
 void MenuLayer::menuCloseCallback(CCObject* pSender)
 {
     CCDirector::sharedDirector()->end();
@@ -211,17 +119,6 @@ void MenuLayer::menuCloseCallback(CCObject* pSender)
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
-=======
-bool MenuLayer::init(){
-	//CCSize winSize = CCDirector::sharedDirector()->getWinSize();
-	if (!CCLayer::init())
-		return false;
-
-	this->initBackground();
-	this->initMenu();
-
-	return true;
->>>>>>> master
 }
 
 void MenuLayer::menuStart(CCObject* pSender)
@@ -231,10 +128,6 @@ void MenuLayer::menuStart(CCObject* pSender)
 //	testLabel->setPosition(ccp(size.width / 2, size.height / 3));
 //	this->addChild(testLabel, 2);
 
-<<<<<<< HEAD
-=======
-void MenuLayer::menuStart(CCObject* pSender){
->>>>>>> master
 	CCDirector::sharedDirector()->replaceScene(GameLayer::scene());
 }
 
@@ -246,7 +139,6 @@ void MenuLayer::menuHelp(CCObject *pSender)
 	this->addChild(testLabel, 2);
 }
 
-<<<<<<< HEAD
 void MenuLayer::menuSetting(CCObject *pSender)
 {
 	CCLabelTTF *testLabel = CCLabelTTF::create("Setting", "Jenna Sue", 30);
@@ -254,20 +146,3 @@ void MenuLayer::menuSetting(CCObject *pSender)
 	testLabel->setPosition(ccp(size.width / 2, size.height / 4));
 	this->addChild(testLabel, 2);
 }
-=======
-void MenuLayer::menuCloseCallback(CCObject* pSender)
-{
-    CCDirector::sharedDirector()->end();
-}
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> master
--- MenuLayer.h
+++ MenuLayer.h
@@ -10,7 +10,6 @@
 
 #include "cocos2d.h"
 #include "GameLayer.h"
-<<<<<<< HEAD
 
 class MenuLayer : public cocos2d::CCLayer
 {
@@ -27,25 +26,6 @@ public:
 	void menuStart(CCObject *pSender);
 	void menuHelp(CCObject *pSender);
 	void menuSetting(CCObject *pSender);
-=======
-using namespace cocos2d;
-class MenuLayer : public CCLayer{
-
-
-private:
-
-	void initBackground();
-	void initMenu();
-
-public:
-	//MenuLayer();
-	virtual bool init();
-	static CCScene* scene();
-	void menuCloseCallback(CCObject* pSender);
-	void menuStart(CCObject* pSender);
-
-	CREATE_FUNC(MenuLayer);
->>>>>>> master
 
 	CREATE_FUNC(MenuLayer);
 };
