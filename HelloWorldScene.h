e0c30169bb3390d685ed5876ad36cd65962936b3
h1994st <h1994st@gmail.com>
2013-09-22 19:43:14 +0800

    Merge……3
    硬是要我全部提交……

--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -2,9 +2,6 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
-#include "Box2D.h"
-#include "Paddle.h"
-#define PTM_RATIO 32.0
 
 class HelloWorld : public cocos2d::CCLayer
 {
@@ -20,14 +17,8 @@ public:
 
     // implement the "static node()" method manually
     CREATE_FUNC(HelloWorld);
-    void tick(float dt);
-    b2World *_world;
-    b2Body *ball_body;
-    b2Body *paddle_body;
-    cocos2d::CCSprite *_ball;
-    Paddle *_paddle;
 
-
+    cocos2d::CCSprite *ball;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
